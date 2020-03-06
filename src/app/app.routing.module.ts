189fa91b7aee3988706d785f5c0353a5535c8ce2
YolandaXQY <xqy2016smile@gmail.com>
2020-03-06 14:29:40 +0800

    完整mortgageCalculator.json的页面加载

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { FormqlEndDataComponent } from './showcase/formql-end-data/formql-end-data.component';
 
 const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: '/formql/contactInfo/123/edit'},
-    { path: 'formql/:name/:id/edit', component: FormqlEndDataComponent}
+    { path: '', pathMatch: 'full', redirectTo: '/formql/contactInfo/testID'},
+    { path: 'formql/:name/:id', component: FormqlEndDataComponent}
 ]
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
